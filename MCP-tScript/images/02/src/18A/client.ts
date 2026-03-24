104ab1732d89a84ae13a3dc0643179a9cdc73b71
stan alam <stan.alam@gmail.com>
2026-03-24 11:50:12 -0400

    added to 18 A source

--- MCP-tScript/images/02/src/18A/client.ts
+++ MCP-tScript/images/02/src/18A/client.ts
@@ -13,3 +13,25 @@ console.log("Debug client starting at server path: ", serverPath);
 const child = spawn('node', [serverPath]);
 
 let serverPath = join(__dirname, 'server.js');
+console.log("DEBUG client starting server at: ", serverPath);
+
+const child = spawn('node', [serverPath]);
+
+let initialized = false;
+//listen for data from the child 
+
+function handleRpcMessage(data) {
+    console.log("handleRcpMessage");
+
+    let message = geRpcMessage(data.toString().trim());
+    if (!message) {
+        console.error("Invalid JSON-RPC message: ", data.toString());
+        return;
+    }
+    console.log("message.result: ", message.result);
+}
+
+function listTools(){
+    console.log("DEBUG client asks for tools list");
+        child.stdin.write(JSON.stringify(listToolsMessage()) + '\n');
+}
